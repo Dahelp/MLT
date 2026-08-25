5b277544b5b62904acac376625b6d4d87a7c6476
Dahelp <oooitscenter@gmail.com>
2026-08-26 00:47:06 +0300

    fix: complete localized planner redesign

--- app/[locale]/plan/page.tsx
+++ app/[locale]/plan/page.tsx
@@ -1,1 +1,6 @@
-export { default } from "../../plan/page";
+import PlanPage from "../../plan/page";
+
+export default async function LocalizedPlanPage({ params }: { params: Promise<{ locale: "en" | "de" | "ru" }> }) {
+  const { locale } = await params;
+  return <PlanPage initialLocale={locale} />;
+}
